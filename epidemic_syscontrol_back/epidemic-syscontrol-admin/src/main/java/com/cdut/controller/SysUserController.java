d713deb8ec5d8eb51dedc6d33505a371493e8c60
首席膜导师 <473457308@qq.com>
2022-10-03 13:07:44 +0800

    新增管理员信息查询接口

--- epidemic_syscontrol_back/epidemic-syscontrol-admin/src/main/java/com/cdut/controller/SysUserController.java
+++ epidemic_syscontrol_back/epidemic-syscontrol-admin/src/main/java/com/cdut/controller/SysUserController.java
@@ -1,6 +1,10 @@
 package com.cdut.controller;
 
 import com.cdut.epidemicsyscontrolcommon.utils.AjaxResult;
+import com.cdut.epidemicsyscontrolcommon.utils.JWTUtil;
+import com.cdut.epidemicsyscontrolcommon.utils.RedisCache;
+import com.cdut.epidemicsyscontrolframework.security.SecurityUtil;
+import com.cdut.epidemicsyscontrolsystem.pojo.LoginUser;
 import com.cdut.epidemicsyscontrolsystem.pojo.SysUser;
 import com.cdut.epidemicsyscontrolsystem.service.RoleService;
 import com.cdut.epidemicsyscontrolsystem.service.SysUserRoleService;
@@ -8,9 +12,7 @@ import com.cdut.epidemicsyscontrolsystem.service.SysUserService;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
@@ -23,6 +25,25 @@ public class SysUserController {
 
     @Autowired
     SysUserRoleService sysUserRoleService;
+
+    @Autowired
+    RedisCache redisCache;
+
+    /**
+     * 管理员信息
+     * @return
+     */
+    @GetMapping("/sysuser/info")
+    public AjaxResult info(){
+        LoginUser loginUser = SecurityUtil.getLoginUser();
+        SysUser user = loginUser.getUser();
+        user.setPassword(null);
+        user.setSalt(null);
+        user.setAvatarUrl(null);
+        loginUser.setUser(user);
+        return AjaxResult.success("管理员信息查询成功",loginUser);
+    }
+
 
     // TODO: 2022/9/1 更新管理员信息
     @RequestMapping(value = "/sysuser/update",method = POST)

1ddaa7f791d6c750024b52d9ec1495a0e46f34ab
kisaragi <3476474034@qq.com>
2022-10-03 19:32:35 +0800

    新增查询请求状态接口（已通过、审核中、已拒绝） 新增行程查询接口 新增req_id查记录接口

--- epidemic_back/epidemic-admin/src/main/java/com/cdut/controller/ReplyController.java
+++ epidemic_back/epidemic-admin/src/main/java/com/cdut/controller/ReplyController.java
@@ -1,5 +1,6 @@
 package com.cdut.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.cdut.epidemic_common.utils.AjaxResult;
 import com.cdut.epidemicsystem.pojo.*;
 import com.cdut.epidemicsystem.service.impl.ReplyServiceImpl;
@@ -10,10 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -84,12 +82,68 @@ public class ReplyController {
         return AjaxResult.success("回复成功", replyService.save(reply));
     }
 
+    @Operation(description = "请求查回复")
+    @RequestMapping(value = "/rep/status", method = GET)
+    AjaxResult postRequest(@RequestParam("type") Integer type,
+                           @RequestParam("requestId") Integer requestId) {
+        int status=0;
+        QueryWrapper<Reply>qw=new QueryWrapper<Reply>();
+        qw.eq("type", type );
+        qw.eq("request_id", requestId );
+        Reply reply=replyService.getOne(qw);
+        System.out.println(reply);
+        if(reply==null){
+            return AjaxResult.success("审核中", reply);
+        }else if(reply.getPassed()==1){
+            return AjaxResult.success("已通过",reply);
+        }else {
+            return AjaxResult.success("已拒绝", reply);
+        }
+    }
+
+
     @Operation(description = "查看全部回复")
     @RequestMapping(value = "/rep/all", method = GET)
     AjaxResult postRequest() {
         log.info("进入user_system，postRequest方法");
         return AjaxResult.success("查询成功", replyService.list());
     }
+
+    @Operation(description = "用户ID查行程")
+    @RequestMapping(value = "/rep/trav", method = GET)
+    AjaxResult getTrav(@RequestParam("id")Integer id) {
+        List<Reply> list = replyService.getList(id);
+        return AjaxResult.success("查询成功", list);
+    }
+
+    @Operation(description = "更新请求状态")
+    @RequestMapping(value = "/rep/uppass", method = POST)
+    AjaxResult uppass(@RequestParam("reply_id") Integer id,
+                      @RequestParam("passed")Integer passed) {
+        Reply reply = replyService.getById(id);
+        if (reply==null){
+            return AjaxResult.error("没有这条记录!");
+        }
+        reply.setPassed(passed);
+        return AjaxResult.success("更新成功", replyService.saveOrUpdate(reply));
+    }
+
+    @Operation(description = "更新回复(可新建 不推荐)")
+    @RequestMapping(value = "/rep/uprep", method = POST)
+    AjaxResult uppass(@RequestBody Reply reply) {
+        return AjaxResult.success("更新成功", replyService.saveOrUpdate(reply));
+    }
+
+    @Operation(description = "rep_id查回复")
+    @RequestMapping(value = "/rep/findId/{rep_id}", method = GET)
+    AjaxResult uppass(@PathVariable("rep_id")String id) {
+        Reply byId = replyService.getById(id);
+        if (byId==null){
+            return AjaxResult.error("记录不存在");
+        }
+        return AjaxResult.success("查询成功", byId);
+    }
+
 
     @GetMapping("/rep/page")
     public AjaxResult findPage(@RequestParam Integer pageNum,

7b61b7db16501d905c11df23f5437b0670353a6f
首席膜导师 <473457308@qq.com>
2023-06-02 09:35:11 +0800

    finish change

--- epidemic_back/epidemic-admin/src/test/java/com/cdut/UserTest.java
+++ epidemic_back/epidemic-admin/src/test/java/com/cdut/UserTest.java
@@ -12,23 +12,23 @@ import java.util.HashMap;
 @SpringBootTest
 public class UserTest {
 //    private RestTemplate restTemplate = new RestTemplate();
-    private String host = "localhost:";
-    private int port = 8088;
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
-
-    @Test
-    public void userGetIdTest(){
-        String url = host + port + "/user/getid";
-        HashMap<String, Object> param = new HashMap<>();
-        param.put("id", 58);
-        String s = HttpUtil.get(url, param);
-        log.info("user by id 58: " + s);
-    }
-    @Test
-    public void getAllUsers(){
-        String url = host + port + "/getAllUsers";
-        log.info(HttpUtil.get(url));
-    }
+//    private String host = "localhost:";
+//    private int port = 8088;
+//    private final Logger log = LoggerFactory.getLogger(this.getClass());
+//
+//    @Test
+//    public void userGetIdTest(){
+//        String url = host + port + "/user/getid";
+//        HashMap<String, Object> param = new HashMap<>();
+//        param.put("id", 58);
+//        String s = HttpUtil.get(url, param);
+//        log.info("user by id 58: " + s);
+//    }
+//    @Test
+//    public void getAllUsers(){
+//        String url = host + port + "/getAllUsers";
+//        log.info(HttpUtil.get(url));
+//    }
 
 
 

9964a8d6facf333144ec54e029b84c8468f8d6cd
hoshinojyunn <85017683+hoshinojyunn@users.noreply.github.com>
2023-06-02 09:55:14 +0800

    Master (#26)
    * 完成初始化基本功能
    
    * 封装响应体
    
    * 完成响应封装
    
    * 修复idea文件bug,增加分页
    
    * 架构优化
    
    * 架构再优化
    
    * README
    
    * README
    
    * 增加后台管理
    
    * 增加后台管理模块
    
    * 后台管理新增权限系统
    
    * 后台管理完成登录登出、处理异常功能
    
    * 完成头像上传及其获取功能，完成后台接口设计
    
    * 完成部署前置工作
    
    * 修复遗漏
    
    * 修复遗漏
    
    * 增加根据时间查询回复
    
    * 完成部署工作
    
    * 更新匿名访问规则
    
    * 更新匿名访问规则
    
    * 更新匿名访问规则
    
    * 我就不信了（
    
    * 我就不信了（
    
    * 同步登录与注册的加密验证，修复新注册用户无法登录的bug
    
    * 新增管理员信息查询接口
    
    * 新增处理状态查询
    
    * 完善管理员信息更改查询流程
    
    * finish change
    
    * finish change
    
    * finish change
    
    * finish change
    
    * change

---
+++ epidemic_back/epidemic-admin/src/test/java/com/cdut/UserTest.java
@@ -0,0 +1,35 @@
+package com.cdut;
+
+import cn.hutool.http.HttpUtil;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+
+@SpringBootTest
+public class UserTest {
+//    private RestTemplate restTemplate = new RestTemplate();
+//    private String host = "localhost:";
+//    private int port = 8088;
+//    private final Logger log = LoggerFactory.getLogger(this.getClass());
+//
+//    @Test
+//    public void userGetIdTest(){
+//        String url = host + port + "/user/getid";
+//        HashMap<String, Object> param = new HashMap<>();
+//        param.put("id", 58);
+//        String s = HttpUtil.get(url, param);
+//        log.info("user by id 58: " + s);
+//    }
+//    @Test
+//    public void getAllUsers(){
+//        String url = host + port + "/getAllUsers";
+//        log.info(HttpUtil.get(url));
+//    }
+
+
+
+}
